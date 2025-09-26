130f2d3e6611c4892213258caf197e09833e2647
Steffen Schubert <steff.schubert@gmx.de>
2025-09-26 19:46:37 +0200

    Anecdote good enough

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,20 @@ import {IconName} from "@/util/icon-utils";
 export default function Home() {
   const sayHello = useCallback(() => {
     window.alert("du hurensohn");
-  }, [])
+  }, []);
 
   return (
     <main className={"min-w-screen flex justify-center bg-primary"}>
-      <div className="max-w-(--width-content) w-(--width-content) overflow-visible min-h-screen flex flex-col gap-1">
+      <div className="max-w-(--width-content) w-(--width-content) overflow-hidden min-h-screen flex flex-col gap-1 items-center">
         <p className="text-primary-foreground">Moin</p>
         <Button onClick={sayHello} variant={"default"}>Drück mich nicht</Button>
-        <div className={"w-full bg-black p-10 flex justify-center gap-5"}>
+        <div className={"overflow-hidden w-1000 bg-black p-10 flex justify-center gap-5"}>
           <TechAnecdote icon={IconName.TYPESCRIPT} color={"#3178c6"} title={"TypeScript"} anecdote={"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam."}/>
           <TechAnecdote icon={IconName.TYPESCRIPT} color={"#3178c6"} title={"TypeScript"} anecdote={"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam."}/>
+          <TechAnecdote icon={IconName.TYPESCRIPT} color={"#3178c6"} title={"Tyript"} anecdote={"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam."}/>
+          <TechAnecdote icon={IconName.TYPESCRIPT} color={"#3178c6"} title={"TypeScript"} anecdote={"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam."}/>
+          <TechAnecdote icon={IconName.TYPESCRIPT} color={"#3178c6"} title={"TypeSsdsa asfd acript"} anecdote={"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam."}/>
+          <TechAnecdote icon={IconName.TYPESCRIPT} color={"#3178c6"} title={"Type asdffsd aScript"} anecdote={"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam."}/>
         </div>
       </div>
     </main>
--- src/components/ui/techAnecdote.tsx
+++ src/components/ui/techAnecdote.tsx
@@ -16,18 +16,20 @@ function TechAnecdote({icon, title, anecdote}: Props) {
   const colorClass = getIconTailwindClass(icon);
 
   return (
-    <div ref={ref} className={cn("flex shadow-xs rounded-md overflow-hidden")}>
-      <div className={cn(`basis-3 grow-0 p-4`, colorClass)}>
-        <TechIcon icon={icon}/>
-      </div>
-      <div className={cn("transition-[width, height] duration-1000 flex content-start items-start overflow-hidden", colorClass, hovering ? "w-48 max-h-50" : "max-h-20 w-0")}>
-        <div className={""}>
-          <p className={cn("text-primary p-4 w-48")}>{anecdote}</p>
+    <div ref={ref} className={cn("rounded-md overflow-hidden")}>
+      <div className={cn("flex")}>
+        <div className={cn(`p-4 rounded-l-md`, colorClass)}>
+          <TechIcon icon={icon}/>
         </div>
-      </div>
-      <div className={cn(`bg-primary flex transition-all duration-1000 overflow-hidden`, hovering ? "max-w-0 min-w-0" : "min-w-32 max-w-48")}>
-        <div className={"p-4 w-32"}>
-          <p className={cn("text-primary-foreground")}>{title}</p>
+        <div className={cn("transition-[width, height] ease-in-out duration-1000 flex content-start items-start overflow-hidden", colorClass, hovering ? "w-48 max-h-50" : "max-h-20 w-0")}>
+          <div className={""}>
+            <p className={cn("text-primary p-4 w-48")}>{anecdote}</p>
+          </div>
+        </div>
+        <div className={cn(`bg-primary flex rounded-r-md transition-all ease-in-out duration-1000 overflow-hidden`, hovering ? "max-w-0 min-w-0" : "min-w-32 max-w-48")}>
+          <div className={"p-4 w-32"}>
+            <p className={cn("text-primary-foreground")}>{title}</p>
+          </div>
         </div>
       </div>
     </div>
