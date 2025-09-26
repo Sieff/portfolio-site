7964f7f016768622273935d0deb7942b9f723d0c
Steffen Schubert <steff.schubert@gmx.de>
2025-09-27 00:52:14 +0200

    bg color

--- src/components/ui/anecdotes/techAnecdote.tsx
+++ src/components/ui/anecdotes/techAnecdote.tsx
@@ -17,17 +17,19 @@ function TechAnecdote({icon, title, anecdote}: Props) {
 
   return (
     <div ref={ref} className={cn("rounded-md overflow-hidden")}>
-      <div className={cn("flex")}>
-        <div className={cn(`p-4 rounded-l-md`, colorClass)}>
-          <TechIcon icon={icon}/>
-        </div>
-        <div className={cn("transition-[width, height] ease-in-out duration-1000 flex content-start items-start overflow-hidden", colorClass, hovering ? "w-48 max-h-50" : "max-h-20 w-0")}>
-          <div className={""}>
-            <p className={cn("text-primary p-4 w-48")}>{anecdote}</p>
+      <div className={cn("flex rounded-l-md")}>
+        <div className={cn("flex rounded-l-md", colorClass)}>
+          <div className={cn(`p-4 rounded-l-md`)}>
+            <TechIcon icon={icon}/>
+          </div>
+          <div className={cn("transition-[width, height] ease-in-out duration-1000 flex content-start items-start overflow-hidden", hovering ? "w-48 max-h-50" : "max-h-20 w-0")}>
+            <div className={""}>
+              <p className={cn("text-primary p-4 w-48")}>{anecdote}</p>
+            </div>
           </div>
         </div>
         <div className={cn(`bg-primary flex rounded-r-md transition-all ease-in-out duration-1000 overflow-hidden`, hovering ? "max-w-0 min-w-0" : "min-w-32 max-w-48")}>
-          <div className={"p-4 w-32"}>
+          <div className={"p-4 w-32 h-20 flex items-center"}>
             <p className={cn("text-primary-foreground")}>{title}</p>
           </div>
         </div>
