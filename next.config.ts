6853a627c56449c317d4d16c4b0b44296882ffa4
Steffen Schubert <steff.schubert@gmx.de>
2025-10-17 15:43:58 +0200

    favico; redirect

--- next.config.ts
+++ next.config.ts
@@ -29,6 +29,16 @@ const nextConfig: NextConfig = {
       },
     },
   },
+
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/bert',
+        permanent: true,
+      },
+    ];
+  },
 }
 
 export default nextConfig;
